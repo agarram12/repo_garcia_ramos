8d7b6fd33eece0d12baa8dd6d9be14594ec21850
agarram12 <agarram0711@g.educaand.es>
2024-09-20 17:31:24 +0200

    update contenidos

--- Programacion/ReglaDeTres/src/ReglaDeTres.java
+++ Programacion/ReglaDeTres/src/ReglaDeTres.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 
 public class ReglaDeTres {
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Resolver una regla de tres.");
+		System.out.print("Introduce el digito A: ");
+		float A = scan.nextFloat();
+		System.out.print("Introduce el digito B: ");
+		float B = scan.nextFloat();
+		System.out.print("Introduce el digito C: ");
+		float C = scan.nextFloat();
+		float resultado = (B*C)/A;
+		System.out.println("El resultado de x es: " + resultado);
+		scan.close();
 	}
 }
