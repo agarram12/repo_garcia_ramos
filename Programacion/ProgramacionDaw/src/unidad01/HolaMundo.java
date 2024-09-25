3f81ecf72ce5e1ae8f187f741077b110f14b7485
agarram12 <agarram0711@g.educaand.es>
2024-09-25 17:53:45 +0200

    Nuevo repo

---
+++ Programacion/ProgramacionDaw/src/unidad01/HolaMundo.java
@@ -0,0 +1,12 @@
+package unidad01;
+/**
+ * Clase para mi primer programa java
+ */
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+		System.err.println("Adios Mundo!");
+	}
+
+}
